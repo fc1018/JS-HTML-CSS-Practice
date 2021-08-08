110f96286b33adf79497f8cb4f535118f6fef7e8
Felix <photoninfinity@gmail.com>
2021-08-08 01:20:43 +0100

    Basic login program

---
+++ javascript.info exercises/logicalOperators1.js
@@ -0,0 +1,5 @@
+let age = prompt("What is your age?",);
+
+if (age >= 14 || age <= 90) {
+    alert("You are between 14 and 90 years old!");
+}
---
+++ javascript.info exercises/logicalOperators2.js
@@ -0,0 +1,21 @@
+let user = prompt("Who's there?",);
+
+if (user == "Admin") {
+    let password = prompt("Password?",);
+
+    if (password == "TheMaster") {
+        alert("Welcome!");
+    }
+    else if (typeof password == "string") {
+        alert("Wrong password");
+    }
+    else {
+        alert("Cancelled");
+    }
+}
+else if (typeof user == "string") {
+    alert("I don't know you");
+}
+else {
+    alert("Cancelled");
+}
